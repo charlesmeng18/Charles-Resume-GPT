211b8c48be14d346334f799bac9b4343f2a316a6
Charles Meng <charlesmeng@charless-air.lan>
2024-11-06 08:59:32 -0800

    implemented vector search on chunk table with scores on results

--- convex/search.ts
+++ convex/search.ts
@@ -1,9 +1,9 @@
 // this document performs vector search on the chunks table
-import { action, query } from "./_generated/server";
+import { action, internalQuery, query } from "./_generated/server";
 import { v } from "convex/values";
-import { api, internal } from "./_generated/api";
 import { OpenAI } from "openai";
-
+import { internal } from "./_generated/api";
+import { Doc } from "./_generated/dataModel";
 
 export async function generateQueryEmbedding (question: string) {
     const apiKey = process.env.OPENAI_API_KEY;
@@ -17,21 +17,47 @@ export async function generateQueryEmbedding (question: string) {
         input: question
         });
     const embedding = response.data[0].embedding;
-    console.log(question, embedding )
+    // console.log(question, embedding )
     return embedding    
   }
 
-export const getRelevantChunks = action({
+export const fetchResults = internalQuery({
+    args: { ids: v.array(v.id("chunks")) },
+    handler: async (ctx, args) => {
+      const results = [];
+      for (const id of args.ids) {
+        const doc = await ctx.db.get(id);
+        if (doc === null) {
+          continue;
+        }
+        results.push(doc);
+      }
+    //   console.log(results)
+      return results;
+    },
+  });
+
+
+export const embeddingsRetriever = action({
     args: {
         question: v.string()
     },
     async handler(ctx, args) {
-        const embedding = await generateQueryEmbedding(args.question);
-        console.log(embedding)
+        const embedding = await generateQueryEmbedding(args.question);        // perform vector search on the chunks table
         const results = await ctx.vectorSearch("chunks", "byEmbedding", {
             vector: embedding,
             limit: 10
-        })
-        return results;
+        });
+        // fetch the document data for each result
+        const chunks : Array<Doc<"chunks">> = await ctx.runQuery(
+            internal.search.fetchResults,
+            { ids: results.map((chunk) => chunk._id)} 
+        );
+        const filteredChunks = chunks.map((chunk, index) => ({
+            summary: chunk.summary,
+            text: chunk.text,
+            score: results[index]._score // Assuming results and chunks are in the same order
+        }));
+        return filteredChunks; // return the filtered chunks with scores
     }
 })    
