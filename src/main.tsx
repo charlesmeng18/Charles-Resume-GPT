c042019e5dae269142db5d7808bbb81798ec9514
Charles Meng <charlesmeng@charless-air.lan>
2024-11-13 10:48:32 -0800

    changed to prod react client

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ConvexReactClient } from "convex/react";
 import { ConvexAuthProvider } from "@convex-dev/auth/react";
 
-const convex = new ConvexReactClient("https://youthful-flamingo-888.convex.cloud");
+const convex = new ConvexReactClient("https://youthful-flamingo-888.convex.cloud");  
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
