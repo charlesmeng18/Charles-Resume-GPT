fc54cf521d9debe545faa1a83a811a589e93d4db
Charles Meng <charlesmeng@Charless-MacBook-Air.local>
2024-11-14 18:49:32 -0800

    added callback URL

--- convex/auth.ts
+++ convex/auth.ts
@@ -4,4 +4,13 @@ import { convexAuth } from "@convex-dev/auth/server";
 
 export const { auth, signIn, signOut, store } = convexAuth({
   providers: [GitHub, Resend],
+  callbacks: {
+    async redirect({redirectTo}) {
+      // check if redirectTo is valid
+      if (!redirectTo) {
+        throw new Error("Invalid redirect URL");
+      }
+      return process.env.SITE_URL || "";
+    }
+  }
 });
