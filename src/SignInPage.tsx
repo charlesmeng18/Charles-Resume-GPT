5dfb8bddf62915f6b3e5152d2d394dc806e1ff80
Charles Meng <charlesmeng13@gmail.com>
2024-11-28 03:27:18 -0800

    slightly modified landing page description intro

--- src/SignInPage.tsx
+++ src/SignInPage.tsx
@@ -10,8 +10,8 @@ export function SignInPage() {
             <h1 className="text-3xl font-bold mb-4">
               Welcome! Charles' Resume Assistant here 👋
             </h1>
-            <div className="text-left">
-              I'm a Chat Assistant, created by Charles to highlight his experience and skills in AI/ML, Search, and Product Management. <br />
+            <div className="text-left mb-4">
+              I'm a Chat Assistant, created by Charles to highlight his experience and skills in Product Management, Search, AI/ML, Enterprise SAAS, and general passion for building things, like programming this web app himself! <br />
               <br />
               Sign in below to start chatting!
             </div>
