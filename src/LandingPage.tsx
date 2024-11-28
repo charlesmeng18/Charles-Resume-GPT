8508fd94f3295275198c71389d076f4b5fd69d03
Charles Meng <charlesmeng13@gmail.com>
2024-11-28 12:15:02 -0800

    added transparent background favicon

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -5,7 +5,7 @@ export function LandingPage() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Hero section with SignInPage */}
-      <div className="flex-1 flex items-center justify-center px-4 pb-8">
+      <div className="flex-1 flex items-center justify-center px-4 pb-16">
         <div className="w-full max-w-lg mt-16 md:mt-24 lg:mt-32">
           <SignInPage />
         </div>
